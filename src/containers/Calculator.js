d9e3782060c74b954ea1271cf9ea58c70098d599
goamoth <geo_en7@yahoo.com>
2020-06-07 23:51:52 +0300

    Added state to the Calculator.js container component

--- src/containers/Calculator.js
+++ src/containers/Calculator.js
@@ -5,6 +5,19 @@ import Results from '../components/Results';
 import '../scss/Calculator.scss';
 
 class Calculator extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         grossPay: 0,
+         nssf: 0,
+         totalDeductions: 0,
+         taxablePay: 0,
+         relief: 0,
+         paye: 0
+      };
+      
+   }
+
    render() {
       return (
          <div className="row">
