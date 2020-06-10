b8a5b4dd98ccaa8fc96f6dc4d09e1906c4c65b41
goamoth <geo_en7@yahoo.com>
2020-06-10 12:42:15 +0300

    Updates

---
+++ precache-manifest.245447a85a226a8bff905bb943c6712d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4d2bf9a517d5e77a86f03745ec5d98",
+    "url": "/payecalculator/index.html"
+  },
+  {
+    "revision": "6b2b667a0cc454ec7264",
+    "url": "/payecalculator/static/css/main.8f499f8d.chunk.css"
+  },
+  {
+    "revision": "f1b6d05468c01eaf27b5",
+    "url": "/payecalculator/static/js/2.4596f424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/payecalculator/static/js/2.4596f424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2b667a0cc454ec7264",
+    "url": "/payecalculator/static/js/main.2d8dfdf6.chunk.js"
+  },
+  {
+    "revision": "55a77a772571041b2850",
+    "url": "/payecalculator/static/js/runtime-main.70b62fc7.js"
+  }
+]);
